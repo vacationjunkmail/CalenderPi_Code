52bfe37950eb940e71ea1000eb8665e030d4d562
Junk Mail <vacationjunkmail@gmail.com>
2020-04-18 19:10:28 -0400

    Changed the script to automatically get the home directory and send the file

--- bash/move_twitter.sh
+++ bash/move_twitter.sh
@@ -1,10 +1,39 @@
 #!/usr/bin/env bash
 
-directory='/home/pi/Downloads/';
-for f in $directory*twitter.txt
-do
- #rsync -e ssh -hav --remove-source-files $f pi@192.168.1.105:/home/pi/Downloads 
-  rsync -hav --remove-source-files $f pi@192.168.1.105:/home/pi/Downloads/
- echo "$f"
-done
+home_path=$HOME
 
+alias_file="$home_path/.bashrc_aliases";
+directory="$home_path/Downloads/";
+
+rsync_addr=''
+reg_ex="'ssh (.*)'"
+
+while IFS= read -r line
+do
+  if [[ "$line" == *"FirstPi"* ]]; then
+    rsync_addr="$line"
+  fi
+done < $alias_file
+
+c=${#rsync_addr}
+z=0
+
+if [ -z "$rsync_addr" ]; then
+	echo "$rsync_addr ______"
+fi
+
+
+[[ $rsync_addr =~ $reg_ex ]]
+
+rsync_addr=${BASH_REMATCH[1]};
+if [ -n "$rsync_addr" ]; then
+	for f in $directory*twitter.txt
+		do
+			echo $f
+	 		##rsync -e ssh -hav --remove-source-files $f pi@192.168.1.105:/home/pi/Downloads 
+			rsync -hav --remove-source-files $f pi@$rsync_addr:/home/pi/Downloads/
+			#echo "$f"
+	done
+else
+	echo "No alias found with with the name of FirstPi"
+fi
